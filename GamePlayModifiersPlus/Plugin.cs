b4e6b12fc3e188fbad4c269ffb4c0170c8fc95ac
Kyle1413 <kylet1412@gmail.com>
2019-01-20 12:14:44 -0500

    Version

--- GamePlayModifiersPlus/Plugin.cs
+++ GamePlayModifiersPlus/Plugin.cs
@@ -21,8 +21,8 @@
         internal static BS_Utils.Utilities.Config ChatConfigSettings = new BS_Utils.Utilities.Config("GameplayModifiersPlus");
         public string Name => "GameplayModifiersPlus";
 
-        public string Version => "1.6.0";
-        public static string pluginVersion = "1.6.0";
+        public string Version => "1.6.1";
+        public static string pluginVersion = "1.6.1";
 
         public static float timeScale = 1;
         Multiplayer.MultiMain multi = null;
